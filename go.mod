ac7fb5ad8d2bfa1502fa63718296fdbfe506c1e1
Ihor Koniukhov <koniukhov@bubuntu-lx>
2024-01-08 18:02:56 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module imageAploaderS3
+
+go 1.21
+
+require (
+	github.com/aws/aws-sdk-go v1.49.4
+	github.com/joho/godotenv v1.5.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/joho/godotenv"
+	"html/template"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("The API has started.")
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/upload", uploadFileHandler)
+	err = http.ListenAndServe(os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./templates/upload.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		err := r.ParseMultipartForm(10 << 20)
+		if err != nil {
+			log.Println(err)
+		}
+		file, handler, err := r.FormFile("myFile")
+		if err != nil {
+			fmt.Println("Error Retrieving the File")
+			fmt.Println(err)
+			return
+		}
+		defer func(file multipart.File) {
+			err := file.Close()
+			if err != nil {
+
+			}
+		}(file)
+
+		sess, _ := session.NewSession(&aws.Config{
+			Region: aws.String(os.Getenv("S3_REGION")),
+		})
+		uploader := s3manager.NewUploader(sess)
+		fileKey := fmt.Sprintf("%s-%s", time.Now().Format("20060102-150405"), handler.Filename)
+
+		_, err = uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(os.Getenv("S3_BUCKET")),
+			Key:    aws.String(fileKey),
+			Body:   file,
+		})
+
+		if err != nil {
+			fmt.Println("Failed to upload", err)
+			return
+		}
+
+		time.Sleep(time.Millisecond * 300)
+
+		_, err = fmt.Fprintf(w, "Successfully uploaded %s%s\n", os.Getenv("SOURCE_URL"), fileKey)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = fmt.Fprintf(w, "Successfully uploaded to resized %sresized/%s\n", os.Getenv("SOURCE_URL"), fileKey)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := fmt.Fprint(w, "Only POST method is supported")
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
